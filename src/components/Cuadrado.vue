20ec38a62dcf480b09b1574b4de806c68b9d3e67
AndreaCuriel <acurielipn@hotmail.com>
2022-03-27 14:50:35 -0600

    first commit

---
+++ src/components/Cuadrado.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="cuadrado">
+      {{$store.state.contador}} <sup>2</sup> = {{$store.getters.cuadrado}}
+    </div>
+
+    <div>
+      <input type="text" v-model="$store.state.texto"> 
+      <button @click="$store.commit('agregardatos')">Agregar</button>
+      <div>{{$store.state.texto}}</div>
+    </div>
+
+    <div id="datos">
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Cuadrado'
+    
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,52 @@
-import { createStore } from 'vuex'
+import { DeprecationTypes } from "vue";
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    contador: 0,
+    texto: "",
   },
   getters: {
+    //recupera datos en el estado
+    cuadrado(state) {
+      return state.contador * state.contador;
+    },
   },
   mutations: {
+    // para modificar
+    bajarContador(state, random) {
+      state.contador -= random;
+    },
+    subirContador(state, random) {
+      state.contador += random;
+    },
+
+    agregardatos(state) {
+      let datos = document.getElementById("tablasIP");
+      const info = document.createElement("p");
+      info.textContent = state.texto;
+      datos.appendChild(info);
+    },
   },
   actions: {
+    //solo acciones asincronas como llamar un api
+    async subirContador({ commit }) {
+      const res = await fetch(
+        "https://www.random.org/integers/?num=1&min=1&max=8&col=1&base=10&format=plain&rnd=new"
+      );
+      const results = await res.json();
+      console.log(results);
+      commit("subirContador", results);
+    },
+
+    async bajarContador({ commit }) {
+      const res = await fetch(
+        "https://www.random.org/integers/?num=1&min=1&max=8&col=1&base=10&format=plain&rnd=new"
+      );
+      const results = await res.json();
+      console.log(results);
+      commit("bajarContador", results);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
